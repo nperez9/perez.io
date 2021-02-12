2eec266c209cd0dad257e1bbd18b22fd861f98f9
unknown <nicolasperez95.a@gmail.com>
2021-02-12 05:14:04 -0300

    Updates

---
+++ precache-manifest.8ce839eeb216b21a82876f59994ca147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90a73677fb897c5d5d178f636fda976",
+    "url": "/perez.io/index.html"
+  },
+  {
+    "revision": "cdac6b4c847955b28402",
+    "url": "/perez.io/static/js/2.b6baeabc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/perez.io/static/js/2.b6baeabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1165c5d0f06f5255a583",
+    "url": "/perez.io/static/js/main.6d6bfa98.chunk.js"
+  },
+  {
+    "revision": "c17cc7bd40be23cb78ff",
+    "url": "/perez.io/static/js/runtime-main.a6641b29.js"
+  }
+]);
