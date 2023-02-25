4f5da584799a887d558be4c360ac74b7f7c9417c
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2023-02-25 05:25:05 -0300

    feat: added container

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,14 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 
-const about = () => {
-  return <div>your component about</div>;
+import { container } from '../../library';
+
+const style = {
+  ...container
 };
 
-export default about;
+const About = () => {
+  return <main css={style}>your component about</main>;
+};
+
+export default About;
