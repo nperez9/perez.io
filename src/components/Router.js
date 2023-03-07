83740110a187f1ee80c2d444835f59f9308bccc1
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2023-03-07 16:09:01 -0300

    feat: updated some ts

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,14 +9,14 @@ import Detail from './detail/Detail';
 
 const router = () => {
   return (
-      <Switch>
-        <Route path="/home" exact component={Home} />      
-        <Route path="/about" exact component={About} />
-        <Route path="/portfolio" exact component={Portfolio} />
-        <Route path="/portfolio/detail/:id" exact component={Detail} />
-        <Route path="/contact" exact component={Contact} />
-        <Redirect exact from="/" to="/home" />
-      </Switch>
+    <Switch>
+      <Route path="/home" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      <Route path="/portfolio" exact component={Portfolio} />
+      <Route path="/portfolio/detail/:id" exact component={Detail} />
+      <Route path="/contact" exact component={Contact} />
+      <Redirect exact from="/" to="/home" />
+    </Switch>
   );
 };
 
