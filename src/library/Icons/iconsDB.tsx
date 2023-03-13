2f79cd372a025162f48480e706327af9e163fdf8
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2023-03-13 16:17:03 -0300

    feat: added icons

--- src/library/Icons/iconsDB.tsx
+++ src/library/Icons/iconsDB.tsx
@@ -168,7 +168,65 @@ export const iconsDB = {
       ></path>
     </svg>
   ),
-  github: <svg></svg>,
-  gmail: <svg></svg>,
-  stackoverflow: <svg></svg>,
+  github: (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="white">
+      <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+    </svg>
+  ),
+  gmail: (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="16" height="16">
+      <linearGradient id="6769YB8EDCGhMGPdL9zwWa" x1="15.072" x2="24.111" y1="13.624" y2="24.129" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#e3e3e3" />
+        <stop offset="1" stop-color="#e2e2e2" />
+      </linearGradient>
+      <path
+        fill="url(#6769YB8EDCGhMGPdL9zwWa)"
+        d="M42.485,40H5.515C4.126,40,3,38.874,3,37.485V10.515C3,9.126,4.126,8,5.515,8h36.969 C43.874,8,45,9.126,45,10.515v26.969C45,38.874,43.874,40,42.485,40z"
+      />
+      <linearGradient id="6769YB8EDCGhMGPdL9zwWb" x1="26.453" x2="36.17" y1="25.441" y2="37.643" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#f5f5f5" />
+        <stop offset=".03" stop-color="#eee" />
+        <stop offset="1" stop-color="#eee" />
+      </linearGradient>
+      <path fill="url(#6769YB8EDCGhMGPdL9zwWb)" d="M42.485,40H8l37-29v26.485C45,38.874,43.874,40,42.485,40z" />
+      <linearGradient id="6769YB8EDCGhMGPdL9zwWc" x1="3" x2="45" y1="24" y2="24" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#d74a39" />
+        <stop offset="1" stop-color="#c73d28" />
+      </linearGradient>
+      <path
+        fill="url(#6769YB8EDCGhMGPdL9zwWc)"
+        d="M5.515,8H8v32H5.515C4.126,40,3,38.874,3,37.485V10.515C3,9.126,4.126,8,5.515,8z M42.485,8 H40v32h2.485C43.874,40,45,38.874,45,37.485V10.515C45,9.126,43.874,8,42.485,8z"
+      />
+      <linearGradient id="6769YB8EDCGhMGPdL9zwWd" x1="24" x2="24" y1="8" y2="38.181" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-opacity=".15" />
+        <stop offset="1" stop-opacity=".03" />
+      </linearGradient>
+      <path fill="url(#6769YB8EDCGhMGPdL9zwWd)" d="M42.485,40H30.515L3,11.485v-0.969C3,9.126,4.126,8,5.515,8h36.969 C43.874,8,45,9.126,45,10.515v26.969C45,38.874,43.874,40,42.485,40z" />
+      <linearGradient id="6769YB8EDCGhMGPdL9zwWe" x1="3" x2="45" y1="17.73" y2="17.73" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#f5f5f5" />
+        <stop offset="1" stop-color="#f5f5f5" />
+      </linearGradient>
+      <path
+        fill="url(#6769YB8EDCGhMGPdL9zwWe)"
+        d="M43.822,13.101L24,27.459L4.178,13.101C3.438,12.565,3,11.707,3,10.793v-0.278 C3,9.126,4.126,8,5.515,8h36.969C43.874,8,45,9.126,45,10.515v0.278C45,11.707,44.562,12.565,43.822,13.101z"
+      />
+      <linearGradient id="6769YB8EDCGhMGPdL9zwWf" x1="24" x2="24" y1="8.446" y2="27.811" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#e05141" />
+        <stop offset="1" stop-color="#de4735" />
+      </linearGradient>
+      <path
+        fill="url(#6769YB8EDCGhMGPdL9zwWf)"
+        d="M42.485,8h-0.3L24,21.172L5.815,8h-0.3C4.126,8,3,9.126,3,10.515v0.278 c0,0.914,0.438,1.772,1.178,2.308L24,27.459l19.822-14.358C44.562,12.565,45,11.707,45,10.793v-0.278C45,9.126,43.874,8,42.485,8z"
+      />
+    </svg>
+  ),
+  stackoverflow: (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16">
+      <path d="M28.16 32H2.475V20.58H5.32v8.575h19.956V20.58h2.884z" fill="#bcbbbb" />
+      <path
+        d="M8.477 19.8l13.993 2.923.585-2.806-13.993-2.923zm1.832-6.704l12.94 6.04 1.208-2.572-12.94-6.08zm3.586-6.353l10.99 9.12 1.832-2.183-10.99-9.12zM20.99 0l-2.3 1.715 8.536 11.46 2.3-1.715zM8.166 26.27H22.43v-2.845H8.166v2.845z"
+        fill="#f48024"
+      />
+    </svg>
+  ),
 };
