1da089b7be994d95c641ec0517522c12b22b6714
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2023-12-12 02:00:02 -0300

    feat: added lethal

---
+++ src/data/proyects/games/7-lethal-wing.ts
@@ -0,0 +1,14 @@
+import { PortfolioItem } from '../../../types';
+
+export const supperCannonBlast: PortfolioItem = {
+  id: 7,
+  type: 'videogame',
+  name: 'Lethal Wing',
+  tech: 'unity',
+  sortNumber: 0,
+  cover_image: '/images/portfolio/lethal-wing/cover.png',
+  short_description: 'Coming soon!',
+  description: `
+    Coming soon!
+  `,
+};
