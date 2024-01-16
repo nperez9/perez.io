936e72172dc1014263e8c149a49722f40dfaef5c
Nicolas Perez <nicolasperez95.a@gmail.com>
2024-01-16 13:43:24 -0300

    bancan shat?

---
+++ src/data/proyects/games/8-rangeon.ts
@@ -0,0 +1,16 @@
+import { PortfolioItem } from '../../../types';
+
+export const rangeon: PortfolioItem = {
+  id: 8,
+  type: 'videogame',
+  name: 'Rangeon',
+  tech: 'unity',
+  sortNumber: 4,
+  cover_image: '/images/games/rangeon/1.png',
+  description_images: ['/images/games/rangeon/2.png', '/images/games/rangeon/3.png', '/images/games/rangeon/1.png'],
+  short_description: 'Enter the Rangeon! a 2D procedural game with seeds!',
+  description: `
+    Enter in the rageon, pass trought the levels, get the keys, evade the enemies and get to the end of the level!
+  `,
+  github_url: 'https://github.com/nperez9/rangeon',
+};
--- src/data/proyects/games/index.ts
+++ src/data/proyects/games/index.ts
@@ -7,7 +7,17 @@ import { climberPlumber } from './4-climber-plumber';
 import { superDragonsLair } from './5-super-dragons-lair';
 import { greenWild } from './6-greenwild';
 import { lethalWing } from './7-lethal-wing';
+import { rangeon } from './8-rangeon';
 
-export const gamesPortfolio: PortfolioItem[] = [supperCannonBlast, climberPlumber, pokeClon, superDragonsLair, timberChopper, greenWild, lethalWing];
+export const gamesPortfolio: PortfolioItem[] = [
+  supperCannonBlast,
+  climberPlumber,
+  pokeClon,
+  superDragonsLair,
+  timberChopper,
+  greenWild,
+  lethalWing,
+  rangeon,
+];
 
 export default gamesPortfolio;
