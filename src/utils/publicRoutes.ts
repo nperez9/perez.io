22dbe5e2badbe7df53d897eb4980d20214abd98e
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2025-07-29 12:39:45 -0300

    feat: migrated to vite

--- src/utils/publicRoutes.ts
+++ src/utils/publicRoutes.ts
@@ -2,4 +2,4 @@
  * return a resource inside public folder
  * @param {string} publicRoute
  */
-export const publicResource = (publicRoute: string) => `${process.env.PUBLIC_URL}${publicRoute}`;
+export const publicResource = (publicRoute: string) => `${publicRoute}`;
