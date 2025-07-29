9ede14a1e3b2bf6e9eccd0d1d626a4248b746e19
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2025-07-29 13:10:42 -0300

    feat: migrated to vite (#5)

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
