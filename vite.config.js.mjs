99d1efd6d13e88297cbb7b40af2420d5943964de
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2025-07-29 21:42:28 -0300

    feat: updated react

--- vite.config.js.mjs
+++ vite.config.js.mjs
@@ -6,4 +6,13 @@ export default defineConfig({
   build: {
     outDir: 'build', // CRA's default build output
   },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@data': path.resolve(__dirname, 'src/data'),
+      '@library': path.resolve(__dirname, 'src/library'),
+    },
+  },
 });
